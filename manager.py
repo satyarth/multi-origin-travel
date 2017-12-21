05c3a1d84a82cff0123acf9adb3387c476fe9e6f
Dolorousrtur <dolorousrtur@gmail.com>
2017-12-21 22:24:25 +0300

    Solution manager updated

--- manager.py
+++ manager.py
@@ -1,4 +1,29 @@
 
 
 class SolutionManager:
-    def __init__(self):
+
+    def __init__(self, solve_func, solution_processor):
+        self.solve = solve_func
+        self.stopped = False
+        self.solutions = {}
+        self.current_id = 0
+        self.solution_processor = solution_processor
+
+
+
+    def stop_func(self):
+        return self.stopped
+
+    def store_solution(self, solution):
+        self.solutions[self.current_id] = solution
+        self.solution_processor(self.current_id, solution)
+        self.current_id += 1
+
+    def get_links(self, solution):
+        if not solution.routes:
+            return []
+
+        links = []
+
+        # def get
+
