7bddc6757a808488b5f386f4366acd5b925734cf
Dolorousrtur <dolorousrtur@gmail.com>
2017-12-21 19:34:24 +0300

    more fixes in SA

--- solution.py
+++ solution.py
@@ -7,5 +7,9 @@ class Solution:
         self.price = None
 
     def __repr__(self):
-        out = "TO: {0},\tCOME: {1},\tLEAVE: {2}"
-        return out.format(self.destination, self.date_come, self.date_leave)
+        if self.price:
+            return "TO: {0},\tCOME: {1},\tLEAVE: {2}, PRICE: {3}".\
+                format(self.destination, self.date_come, self.date_leave, self.price)
+        else:
+            return "TO: {0},\tCOME: {1},\tLEAVE: {2}". \
+                format(self.destination, self.date_come, self.date_leave)
