f333399d59ca638c1e17687d7ad23e0c3618dbfa
Dolorousrtur <dolorousrtur@gmail.com>
2017-12-21 22:29:35 +0300

    solve methos added to SolutionManager

--- manager.py
+++ manager.py
@@ -3,13 +3,11 @@
 class SolutionManager:
 
     def __init__(self, solve_func, solution_processor):
-        self.solve = solve_func
+        self.solve_func = solve_func
         self.stopped = False
         self.solutions = {}
         self.current_id = 0
         self.solution_processor = solution_processor
-
-
 
     def stop_func(self):
         return self.stopped
@@ -18,6 +16,9 @@ class SolutionManager:
         self.solutions[self.current_id] = solution
         self.solution_processor(self.current_id, solution)
         self.current_id += 1
+
+    def solve(self, outbound_date, inbound_date, origins, min_days):
+        self.solve_func(outbound_date, inbound_date, origins, self.stop_func, self.store_solution, min_days)
 
     def get_links(self, solution):
         if not solution.routes:
